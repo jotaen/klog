4d00db40069c8c0b5fb2b7af8885830dc4d09c49
Jan Heuermann <jan@jotaen.net>
2020-12-06 19:22:20 +0100

    Ensure proper setup and teardown

--- store/store_test.go
+++ store/store_test.go
@@ -8,44 +8,47 @@ import (
 	"testing"
 )
 
-const (
-	TEST_PATH = "../tmp"
-)
-
-func setup() {
-	os.RemoveAll(TEST_PATH)
-	os.MkdirAll(TEST_PATH, os.ModePerm)
+func run(fn func(string)) {
+	path := "../tmp"
+	os.RemoveAll(path)
+	os.MkdirAll(path, os.ModePerm)
+	fn(path)
+	os.RemoveAll(path)
 }
 
 func TestInitialisesFileStoreIfPathExists(t *testing.T) {
-	setup()
-	store, err := CreateFsStore(TEST_PATH)
-	assert.Nil(t, err)
-	assert.NotNil(t, store)
+	run(func(path string) {
+		store, err := CreateFsStore(path)
+		assert.Nil(t, err)
+		assert.NotNil(t, store)
+	})
 }
 
 func TestFailsToInitialiseFileStoreIfPathDoesNotExists(t *testing.T) {
-	setup()
-	store, err := CreateFsStore(TEST_PATH + "/qwerty123")
-	assert.Nil(t, store)
-	assert.Error(t, err)
+	run(func(path string) {
+		store, err := CreateFsStore(path + "/qwerty123")
+		assert.Nil(t, store)
+		assert.Error(t, err)
+	})
 }
 
 func TestGetFailsIfDateDoesNotExist(t *testing.T) {
-	setup()
-	store, _ := CreateFsStore(TEST_PATH)
-	_, errs := store.Get(datetime.Date{Year: 2020, Month: 1, Day: 31})
-	assert.Error(t, errs[0])
+	run(func(path string) {
+		store, _ := CreateFsStore(path)
+		_, errs := store.Get(datetime.Date{Year: 2020, Month: 1, Day: 31})
+		assert.Error(t, errs[0])
+	})
 }
 
 func TestSavePersists(t *testing.T) {
-	setup()
-	date := datetime.Date{Year: 2000, Month: 3, Day: 15}
-	store, _ := CreateFsStore(TEST_PATH)
-	originalWd, _ := workday.Create(date)
-	err := store.Save(originalWd)
-	assert.Nil(t, err)
+	run(func(path string) {
+		date := datetime.Date{Year: 2000, Month: 3, Day: 15}
+		store, _ := CreateFsStore(path)
+		originalWd, _ := workday.Create(date)
+		err := store.Save(originalWd)
+		assert.Nil(t, err)
 
-	readWd, _ := store.Get(date)
-	assert.Equal(t, originalWd, readWd)
+		readWd, _ := store.Get(date)
+		assert.Equal(t, originalWd, readWd)
+	})
 }
