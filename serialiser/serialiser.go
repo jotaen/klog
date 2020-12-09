3bd5c7691ec6b09cc8b1011df889d36220b8a351
Jan Heuermann <jan@jotaen.net>
2020-12-09 20:42:46 +0100

    Serialise ranges

--- serialiser/serialiser.go
+++ serialiser/serialiser.go
@@ -7,10 +7,27 @@ import (
 
 func Serialise(workDay workday.WorkDay) string {
 	text := ""
+
+	// Date
 	text += fmt.Sprintf("date: %v", workDay.Date().ToString())
+
+	// Summary
 	if len(workDay.Summary()) > 0 {
 		text += fmt.Sprintf("\nsummary: %v", workDay.Summary())
 	}
+
+	// Hours
+	if len(workDay.Ranges()) > 0 {
+		text += "\nhours:"
+		for _, rs := range workDay.Ranges() {
+			text += fmt.Sprintf("\n- start: %v", rs[0].ToString())
+			if rs[1] != nil {
+				text += fmt.Sprintf("\n  end: %v", rs[1].ToString())
+			}
+		}
+	}
+
+	// Final newline
 	text += "\n"
 	return text
 }
--- serialiser/serialiser_test.go
+++ serialiser/serialiser_test.go
@@ -24,3 +24,20 @@ func TestSerialiseSummaryIfPresent(t *testing.T) {
 summary: Hello World!
 `, text)
 }
+
+func TestSerialiseRanges(t *testing.T) {
+	date, _ := datetime.CreateDate(1859, 6, 2)
+	workDay := workday.Create(date)
+	time1, _ := datetime.CreateTime(8, 31)
+	time2, _ := datetime.CreateTime(14, 2)
+	workDay.AddRange(time1, time2)
+	time3, _ := datetime.CreateTime(15, 0)
+	workDay.AddOpenRange(time3)
+	text := Serialise(workDay)
+	assert.Equal(t, `date: 1859-06-02
+hours:
+- start: 08:31
+  end: 14:02
+- start: 15:00
+`, text)
+}
