7dfedaeababa1119b6d08605f13899d4c708d26a
Jan Heuermann <jan@jotaen.net>
2021-03-04 09:32:02 +0100

    Implement all entry types

--- src/parser/json/serialiser.go
+++ src/parser/json/serialiser.go
@@ -1,10 +1,12 @@
 package json
 
 import (
+	"bytes"
 	"encoding/json"
 	. "klog"
 	"klog/parser/parsing"
 	"klog/service"
+	"strings"
 )
 
 type Envelop struct {
@@ -20,15 +22,27 @@ type EntryView struct {
 	TotalMins int      `json:"total_mins"`
 }
 
+type OpenRangeView struct {
+	EntryView
+	Start     string `json:"start"`
+	StartMins int    `json:"start_mins"`
+}
+
+type RangeView struct {
+	OpenRangeView
+	End     string `json:"end"`
+	EndMins int    `json:"end_mins"`
+}
+
 type RecordView struct {
-	Date            string      `json:"date"`
-	Summary         string      `json:"summary"`
-	Total           string      `json:"total"`
-	TotalMins       int         `json:"total_mins"`
-	ShouldTotal     string      `json:"should_total"`
-	ShouldTotalMins int         `json:"should_total_mins"`
-	Tags            []string    `json:"tags"`
-	Entries         []EntryView `json:"entries"`
+	Date            string        `json:"date"`
+	Summary         string        `json:"summary"`
+	Total           string        `json:"total"`
+	TotalMins       int           `json:"total_mins"`
+	ShouldTotal     string        `json:"should_total"`
+	ShouldTotalMins int           `json:"should_total_mins"`
+	Tags            []string      `json:"tags"`
+	Entries         []interface{} `json:"entries"`
 }
 
 func ToJson(rs []Record, errs parsing.Errors) string {
@@ -36,37 +50,19 @@ func ToJson(rs []Record, errs parsing.Errors) string {
 		Records: toView(rs),
 		Errors:  errs,
 	}
-	result, err := json.Marshal(&envelop)
+	buffer := new(bytes.Buffer)
+	enc := json.NewEncoder(buffer)
+	enc.SetEscapeHTML(false)
+	err := enc.Encode(&envelop)
 	if err != nil {
 		panic(err) // This should never happen
 	}
-	return string(result)
+	return strings.TrimRight(buffer.String(), "\n")
 }
 
 func toView(rs []Record) []RecordView {
 	result := []RecordView{}
 	for _, r := range rs {
-		entries := func() []EntryView {
-			var evs []EntryView
-			for _, e := range r.Entries() {
-				entryType := e.Unbox(func(r Range) interface{} {
-					return "range"
-				}, func(d Duration) interface{} {
-					return "duration"
-				}, func(openRange OpenRange) interface{} {
-					return "open_range"
-				}).(string)
-				evs = append(evs, EntryView{
-					Type:      entryType,
-					Summary:   e.Summary().ToString(),
-					Tags:      e.Summary().Tags().ToStrings(),
-					Total:     e.Duration().ToString(),
-					TotalMins: e.Duration().InMinutes(),
-				})
-				return evs
-			}
-			return evs
-		}()
 		total := service.Total(r)
 		v := RecordView{
 			Date:            r.Date().ToString(),
@@ -76,9 +72,48 @@ func toView(rs []Record) []RecordView {
 			ShouldTotal:     r.ShouldTotal().ToString(),
 			ShouldTotalMins: r.ShouldTotal().InMinutes(),
 			Tags:            r.Summary().Tags().ToStrings(),
-			Entries:         entries,
+			Entries:         toEntryViews(r.Entries()),
 		}
 		result = append(result, v)
 	}
 	return result
+}
+
+func toEntryViews(es []Entry) []interface{} {
+	var views []interface{}
+	for _, e := range es {
+		base := EntryView{
+			Summary:   e.Summary().ToString(),
+			Tags:      e.Summary().Tags().ToStrings(),
+			Total:     e.Duration().ToString(),
+			TotalMins: e.Duration().InMinutes(),
+		}
+		if base.Tags == nil {
+			base.Tags = []string{}
+		}
+		view := e.Unbox(func(r Range) interface{} {
+			base.Type = "range"
+			return RangeView{
+				OpenRangeView: OpenRangeView{
+					EntryView: base,
+					Start:     r.Start().ToString(),
+					StartMins: r.Start().MidnightOffset().InMinutes(),
+				},
+				End:     r.End().ToString(),
+				EndMins: r.End().MidnightOffset().InMinutes(),
+			}
+		}, func(d Duration) interface{} {
+			base.Type = "duration"
+			return base
+		}, func(o OpenRange) interface{} {
+			base.Type = "open_range"
+			return OpenRangeView{
+				EntryView: base,
+				Start:     o.Start().ToString(),
+				StartMins: o.Start().MidnightOffset().InMinutes(),
+			}
+		})
+		views = append(views, view)
+	}
+	return views
 }
--- src/parser/json/serialiser_test.go
+++ src/parser/json/serialiser_test.go
@@ -22,13 +22,15 @@ func TestSerialiseFullBlownRecord(t *testing.T) {
 		r.SetSummary("Hello #World")
 		r.SetShouldTotal(NewDuration(7, 30))
 		r.AddDuration(NewDuration(1, 30), "#some #thing")
+		r.AddRange(Ɀ_Range_(Ɀ_TimeYesterday_(23, 44), Ɀ_Time_(5, 23)), "")
+		r.StartOpenRange(Ɀ_TimeTomorrow_(0, 28), "Started #todo")
 		return []Record{r}
 	}(), nil)
 	assert.Equal(t, `{"records":[{`+
 		`"date":"2000-12-31",`+
 		`"summary":"Hello #World",`+
-		`"total":"1h30m",`+
-		`"total_mins":90,`+
+		`"total":"7h9m",`+
+		`"total_mins":429,`+
 		`"should_total":"7h30m!",`+
 		`"should_total_mins":450,`+
 		`"tags":["#world"],`+
@@ -38,6 +40,24 @@ func TestSerialiseFullBlownRecord(t *testing.T) {
 		`"tags":["#some","#thing"],`+
 		`"total":"1h30m",`+
 		`"total_mins":90`+
+		`},{`+
+		`"type":"range",`+
+		`"summary":"",`+
+		`"tags":[],`+
+		`"total":"5h39m",`+
+		`"total_mins":339,`+
+		`"start":"<23:44",`+
+		`"start_mins":-16,`+
+		`"end":"5:23",`+
+		`"end_mins":323`+
+		`},{`+
+		`"type":"open_range",`+
+		`"summary":"Started #todo",`+
+		`"tags":["#todo"],`+
+		`"total":"0m",`+
+		`"total_mins":0,`+
+		`"start":"0:28>",`+
+		`"start_mins":1468`+
 		`}]`+
 		`}],"errors":null}`, json)
 }
