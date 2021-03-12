5e801ea05e64cf39d7ff3927e3ec41024ec3e88d
Jan Heuermann <jan@jotaen.net>
2021-03-12 23:33:43 +0100

    Change semantics of `--after` and `--before` and introduce `--since` and `--until`

--- src/app/cli/common_args.go
+++ src/app/cli/common_args.go
@@ -10,22 +10,6 @@ type InputFilesArgs struct {
 	File []string `arg optional type:"existingfile" name:"file" help:".klg source file(s) (if empty the bookmark is used)"`
 }
 
-func (args *FilterArgs) filter(now gotime.Time, rs []Record) []Record {
-	qry := service.FilterQry{
-		BeforeEq: args.BeforeEq,
-		AfterEq:  args.AfterEq,
-		Tags:     args.Tags,
-		Dates:    args.Date,
-	}
-	if args.Today {
-		qry.Dates = append(qry.Dates, NewDateFromTime(now))
-	}
-	if args.Yesterday {
-		qry.Dates = append(qry.Dates, NewDateFromTime(now.AddDate(0, 0, -1)))
-	}
-	return service.Filter(rs, qry)
-}
-
 type DiffArg struct {
 	Diff bool `name:"diff" short:"d" help:"Show difference between actual and should total time"`
 }
@@ -43,12 +27,36 @@ func (args *NowArgs) total(reference gotime.Time, rs ...Record) Duration {
 }
 
 type FilterArgs struct {
-	Tags      []string `name:"tag" help:"Only records (or particular entries) that match this tag"`
-	Date      []Date   `name:"date" help:"Only records at this date"`
-	Today     bool     `name:"today" help:"Only records at today’s date"`
-	Yesterday bool     `name:"yesterday" help:"Only records at yesterday’s date"`
-	AfterEq   Date     `name:"after" help:"Only records after this date (inclusive)"`
-	BeforeEq  Date     `name:"before" help:"Only records before this date (inclusive)"`
+	Tags      []string `name:"tag" group:"Filter" help:"Only records (or particular entries) that match this tag"`
+	Date      []Date   `name:"date" group:"Filter" help:"Only records at this date"`
+	Today     bool     `name:"today" group:"Filter" help:"Only records at today’s date"`
+	Yesterday bool     `name:"yesterday" group:"Filter" help:"Only records at yesterday’s date"`
+	Since     Date     `name:"since" group:"Filter" help:"Only records since this date (inclusive)"`
+	Until     Date     `name:"until" group:"Filter" help:"Only records until this date (inclusive)"`
+	After     Date     `name:"after" group:"Filter" help:"Only records after this date (exclusive)"`
+	Before    Date     `name:"before" group:"Filter" help:"Only records before this date (exclusive)"`
+}
+
+func (args *FilterArgs) filter(now gotime.Time, rs []Record) []Record {
+	qry := service.FilterQry{
+		BeforeEq: args.Until,
+		AfterEq:  args.Since,
+		Tags:     args.Tags,
+		Dates:    args.Date,
+	}
+	if args.After != nil {
+		qry.AfterEq = args.After.PlusDays(1)
+	}
+	if args.Before != nil {
+		qry.BeforeEq = args.Before.PlusDays(-1)
+	}
+	if args.Today {
+		qry.Dates = append(qry.Dates, NewDateFromTime(now))
+	}
+	if args.Yesterday {
+		qry.Dates = append(qry.Dates, NewDateFromTime(now.AddDate(0, 0, -1)))
+	}
+	return service.Filter(rs, qry)
 }
 
 type WarnArgs struct {
