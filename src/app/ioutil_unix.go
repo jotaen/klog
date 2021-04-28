b5e353eb7af01d41d15e85a53b0d4f7b103cb115
Jan Heuermann <jan@jotaen.net>
2021-04-28 18:42:38 +0200

    Display helpful error message on Windows for `bookmark set`
    Since symlinks are not supported out of the box

--- src/app/ioutil_unix.go
+++ src/app/ioutil_unix.go
@@ -2,6 +2,20 @@
 
 package app
 
+import "os"
+
 func flagAsHidden(path string) {
 	// Nothing to do on UNIX
+}
+
+func createSymlinkForBookmark(targetPath string, linkPath string) Error {
+	err := os.Symlink(targetPath, linkPath)
+	if err != nil {
+		return NewError(
+			"Failed to create bookmark",
+			"Unable to create a symlink for the new bookmark",
+			err,
+		)
+	}
+	return nil
 }
--- src/app/ioutil_windows.go
+++ src/app/ioutil_windows.go
@@ -3,6 +3,7 @@
 package app
 
 import (
+	"os"
 	"syscall"
 )
 
@@ -12,4 +13,18 @@ func flagAsHidden(path string) {
 		return
 	}
 	_ = syscall.SetFileAttributes(winFileName, syscall.FILE_ATTRIBUTE_HIDDEN)
+}
+
+func createSymlinkForBookmark(targetPath string, linkPath string) Error {
+	err := os.Symlink(targetPath, linkPath)
+	if err != nil {
+		return NewError(
+			"Failed to create bookmark",
+			"On Windows the `bookmark set` subcommand requires admin privileges or "+
+				"developer mode. (klog needs to create a symlink and Windows doesn’t "+
+				"allow this otherwise.)",
+			err,
+		)
+	}
+	return nil
 }
