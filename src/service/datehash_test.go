b326238cbd804f672745a5cf94c31132af5781aa
Jan Heuermann <jan@jotaen.net>
2021-08-17 18:05:49 +0200

    Calculate different hashes for day, week, month, etc.

--- src/service/datehash_test.go
+++ src/service/datehash_test.go
@@ -7,10 +7,26 @@ import (
 )
 
 func TestHashYieldsDistinctValues(t *testing.T) {
-	hashes := make(map[DayHash]bool)
-	for i, d := 0, Ɀ_Date_(1000, 1, 1); i < 1000; i++ {
-		d = d.PlusDays(i)
-		hashes[NewDayHash(d)] = true
+	dayHashes := make(map[DayHash]bool)
+	weekHashes := make(map[WeekHash]bool)
+	monthHashes := make(map[MonthHash]bool)
+	quarterHashes := make(map[QuarterHash]bool)
+	yearHashes := make(map[YearHash]bool)
+
+	// 1.1.1000 is a Wednesday. 1000 days later it’s Sunday, 27.9.1002
+	initialDate := Ɀ_Date_(1000, 1, 1)
+	for i := 0; i < 1000; i++ {
+		d := initialDate.PlusDays(i)
+		dayHashes[NewDayHash(d)] = true
+		weekHashes[NewWeekHash(d)] = true
+		monthHashes[NewMonthHash(d)] = true
+		quarterHashes[NewQuarterHash(d)] = true
+		yearHashes[NewYearHash(d)] = true
 	}
-	assert.Len(t, hashes, 1000)
+
+	assert.Len(t, dayHashes, 1000)
+	assert.Len(t, weekHashes, 145)
+	assert.Len(t, monthHashes, 33)
+	assert.Len(t, quarterHashes, 11)
+	assert.Len(t, yearHashes, 3)
 }
