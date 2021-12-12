8da3ac3753430af65272f5f985de78170a6ed390
Jan Heuermann <jan@jotaen.net>
2021-12-12 15:49:38 +0100

    Add test for `print` command

--- src/app/commands/print_test.go
+++ src/app/commands/print_test.go
@@ -16,13 +16,36 @@ func TestPrintOutRecord(t *testing.T) {
 	state, err := NewTestingContext()._SetRecords(`
 2018-01-31
 Hello #world
-	1h
+    1h
 `)._Run((&Print{}).Run)
 	require.Nil(t, err)
 	assert.Equal(t, `
 2018-01-31
 Hello #world
     1h
+
+`, state.printBuffer)
+}
+
+func TestPrintOutRecordInCanonicalFormat(t *testing.T) {
+	state, err := NewTestingContext()._SetRecords(`
+2018-01-31
+Hello #world
+  09:00-13:00
+  22:00  -  24:00
+  60m
+  2h0m
+  0h
+`)._Run((&Print{}).Run)
+	require.Nil(t, err)
+	assert.Equal(t, `
+2018-01-31
+Hello #world
+    9:00 - 13:00
+    22:00 - 0:00>
+    1h
+    2h
+    0m
 
 `, state.printBuffer)
 }
