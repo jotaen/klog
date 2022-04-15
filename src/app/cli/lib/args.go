508dba9b0bfa604e2d8621b90731562f0db7cd2c
Steve M. Kim <chairmank@gmail.com>
2022-04-15 00:56:12 -0500

    Custom completion generator that lists *.klg files and klog bookmarks

--- src/app/cli/lib/args.go
+++ src/app/cli/lib/args.go
@@ -12,11 +12,11 @@ import (
 )
 
 type InputFilesArgs struct {
-	File []app.FileOrBookmarkName `arg:"" optional:"" type:"string" name:"file or bookmark" help:".klg source file(s) (if empty the bookmark is used)"`
+	File []app.FileOrBookmarkName `arg:"" optional:"" type:"string" predictor:"file or bookmark" name:"file or bookmark" help:".klg source file(s) (if empty the bookmark is used)"`
 }
 
 type OutputFileArgs struct {
-	File app.FileOrBookmarkName `arg:"" optional:"" type:"string" name:"file or bookmark" help:".klg source file (if empty the bookmark is used)"`
+	File app.FileOrBookmarkName `arg:"" optional:"" type:"string" predictor:"file or bookmark" name:"file or bookmark" help:".klg source file (if empty the bookmark is used)"`
 }
 
 type AtDateArgs struct {
--- src/app/cli/lib/completion.go
+++ src/app/cli/lib/completion.go
@@ -5,6 +5,23 @@ import (
 	"github.com/posener/complete"
 )
 
+func PredictBookmarks(ctx app.Context) complete.Predictor {
+	thunk := func() []string {
+		names := make([]string, 0)
+		bookmarksCollection, err := ctx.ReadBookmarks()
+		if err != nil {
+			return names
+		}
+		for _, bookmark := range bookmarksCollection.All() {
+			names = append(names, bookmark.Name().ValuePretty())
+		}
+		return names
+	}
+	return complete.PredictFunc(func(a complete.Args) []string { return thunk() })
+}
+
 func Predictors(ctx app.Context) map[string]complete.Predictor {
-	return map[string]complete.Predictor{} // TODO
+	return map[string]complete.Predictor{
+		"file or bookmark": complete.PredictOr(complete.PredictFiles("*.klg"), PredictBookmarks(ctx)),
+	}
 }
