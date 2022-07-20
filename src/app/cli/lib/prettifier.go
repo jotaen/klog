650bad56479c49e73d740c156b27425445b71993
Jan Heuermann <jan@jotaen.net>
2022-07-20 07:20:46 +0200

    Rename files, fix unstyled output bug

--- src/app/cli/lib/prettifier.go
+++ src/app/cli/lib/prettifier.go
@@ -3,82 +3,11 @@ package lib
 import (
 	"errors"
 	"fmt"
-	. "github.com/jotaen/klog/src"
 	"github.com/jotaen/klog/src/app"
 	. "github.com/jotaen/klog/src/app/cli/lib/terminalformat"
-	"github.com/jotaen/klog/src/parser"
 	"github.com/jotaen/klog/src/service"
-	"strconv"
 	"strings"
 )
-
-type CliSerialiser struct {
-	Unstyled bool // -> No colouring/styling
-	Decimal  bool // -> Decimal values rather than the canonical totals
-}
-
-func (cs CliSerialiser) format(s Style, t string) string {
-	if cs.Unstyled {
-		return t
-	}
-	return s.Format(t)
-}
-
-func (cs CliSerialiser) duration(d Duration, withSign bool) string {
-	if cs.Decimal {
-		return strconv.Itoa(d.InMinutes())
-	}
-	if withSign {
-		return d.ToStringWithSign()
-	}
-	return d.ToString()
-}
-
-func (cs CliSerialiser) Date(d Date) string {
-	return cs.format(Style{Color: "015", IsUnderlined: true}, d.ToString())
-}
-
-func (cs CliSerialiser) ShouldTotal(d Duration) string {
-	return cs.format(Style{Color: "213"}, cs.duration(d, false))
-}
-
-func (cs CliSerialiser) Summary(s parser.SummaryText) string {
-	txt := s.ToString()
-	style := Style{Color: "249"}
-	hashStyle := style.ChangedBold(true).ChangedColor("251")
-	txt = HashTagPattern.ReplaceAllStringFunc(txt, func(h string) string {
-		return hashStyle.FormatAndRestore(h, style)
-	})
-	return cs.format(style, txt)
-}
-
-func (cs CliSerialiser) Range(r Range) string {
-	return cs.format(Style{Color: "117"}, r.ToString())
-}
-
-func (cs CliSerialiser) OpenRange(or OpenRange) string {
-	return cs.format(Style{Color: "027"}, or.ToString())
-}
-
-func (cs CliSerialiser) Duration(d Duration) string {
-	f := Style{Color: "120"}
-	if d.InMinutes() < 0 {
-		f.Color = "167"
-	}
-	return cs.format(f, cs.duration(d, false))
-}
-
-func (cs CliSerialiser) SignedDuration(d Duration) string {
-	f := Style{Color: "120"}
-	if d.InMinutes() < 0 {
-		f.Color = "167"
-	}
-	return cs.format(f, cs.duration(d, true))
-}
-
-func (cs CliSerialiser) Time(t Time) string {
-	return cs.format(Style{Color: "027"}, t.ToString())
-}
 
 // PrettifyError turns an error into a coloured and well-structured form.
 func PrettifyError(err error, isDebug bool) error {
