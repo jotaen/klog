f7369653294316f4b68e44e667626a3b2e3725ff
Jan Heuermann <jan@jotaen.net>
2022-08-18 14:25:24 +0200

    Switch to `kong-completion` library

--- klog/app/cli/index.go
+++ klog/app/cli/index.go
@@ -3,6 +3,8 @@ Package cli contains handlers for all available commands.
 */
 package cli
 
+import kcmpl "github.com/jotaen/kong-completion"
+
 type Cli struct {
 	// Evaluate
 	Print  Print  `cmd:"" group:"Evaluate" help:"Pretty-prints records"`
@@ -25,11 +27,10 @@ type Cli struct {
 	Bk        Bookmarks `cmd:"" group:"Bookmarks" hidden:"" help:"Alias"`
 
 	// Misc
-	Edit        Edit       `cmd:"" group:"Misc" help:"Opens a file or bookmark in your editor"`
-	Goto        Goto       `cmd:"" group:"Misc" help:"Opens the file explorer at the given location"`
-	Json        Json       `cmd:"" group:"Misc" help:"Converts records to JSON"`
-	Info        Info       `cmd:"" group:"Misc" default:"withargs" help:"Displays meta info about klog"`
-	Version     Version    `cmd:"" group:"Misc" help:"Prints version info and check for updates"`
-	Completion  Completion `cmd:"" group:"Misc" help:"Outputs shell code for initialising tab completions"`
-	Completions Completion `cmd:"" group:"Misc" hidden:"" help:"Alias"`
+	Edit       Edit             `cmd:"" group:"Misc" help:"Opens a file or bookmark in your editor"`
+	Goto       Goto             `cmd:"" group:"Misc" help:"Opens the file explorer at the given location"`
+	Json       Json             `cmd:"" group:"Misc" help:"Converts records to JSON"`
+	Info       Info             `cmd:"" group:"Misc" default:"withargs" help:"Displays meta info about klog"`
+	Version    Version          `cmd:"" group:"Misc" help:"Prints version info and check for updates"`
+	Completion kcmpl.Completion `cmd:"" group:"Misc" help:"Outputs shell code for enabling tab completion"`
 }
