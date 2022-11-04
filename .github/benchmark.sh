29aaff65bddbe1584dbc3a783dd7831fecac4529
Jan Heuermann <jan@jotaen.net>
2022-11-04 12:10:26 +0100

    Make benchmark more differentiated

--- .github/benchmark.sh
+++ .github/benchmark.sh
@@ -2,12 +2,23 @@
 
 set -e
 
-# Generate test data.
-FILE="$(mktemp)"
-go run benchmark.go 10000 > "${FILE}"
+declare -a samples=(1 10 100 1000 10000)
+ITERATIONS=3
 
 # Ensure binary is all set.
 klog > /dev/null
 
 # Run benchmark.
-time klog total --no-warn "${FILE}"
+TIMEFORMAT=%R
+for size in "${samples[@]}"; do
+  printf "%8d:  " $size
+  for _ in $(seq $ITERATIONS); do
+    # Generate new test data.
+    file="$(mktemp)"
+    go run benchmark.go "${size}" > "${file}"
+
+    runtime=$( { time klog total --no-warn "${file}" > /dev/null; } 2>&1 )
+    printf "%ss  " $runtime
+  done
+  echo
+done
