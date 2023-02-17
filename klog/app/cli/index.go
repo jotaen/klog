1b66b0ada8592e60938efce580ee039c69109a91
Jan Heuermann <jan@jotaen.net>
2023-02-17 11:19:52 +0100

    Tidy up main menu

--- klog/app/cli/index.go
+++ klog/app/cli/index.go
@@ -6,32 +6,30 @@ package cli
 import kcmpl "github.com/jotaen/kong-completion"
 
 type Cli struct {
-	// Evaluate
+	// Evaluate Files
 	Print  Print  `cmd:"" group:"Evaluate Files" help:"Pretty-prints records"`
 	Total  Total  `cmd:"" group:"Evaluate Files" help:"Evaluates the total time"`
 	Report Report `cmd:"" group:"Evaluate Files" help:"Prints a calendar report summarising all days"`
 	Tags   Tags   `cmd:"" group:"Evaluate Files" help:"Prints total times aggregated by tags"`
 	Today  Today  `cmd:"" group:"Evaluate Files" help:"Evaluates the current day"`
 
-	// Manipulate
+	// Manipulate Files
 	Track  Track  `cmd:"" group:"Manipulate Files" help:"Adds a new entry to a record"`
 	Start  Start  `cmd:"" group:"Manipulate Files" aliases:"in" help:"Starts a new open time range"`
 	Stop   Stop   `cmd:"" group:"Manipulate Files" aliases:"out" help:"Closes the open time range"`
 	Pause  Pause  `cmd:"" group:"Manipulate Files" help:"Pauses the open time range"`
 	Create Create `cmd:"" group:"Manipulate Files" help:"Creates a new record"`
 
-	// Bookmarks
-	Bookmarks Bookmarks `cmd:"" group:"Bookmarks (bk)" help:"Named aliases for often-used files"`
-	Bookmark  Bookmarks `cmd:"" group:"Bookmarks" hidden:"" help:"Alias"`
-	Bm        Bookmarks `cmd:"" group:"Bookmarks" hidden:"" help:"Alias"`
-	Bk        Bookmarks `cmd:"" group:"Bookmarks" hidden:"" help:"Alias"`
+	// Manage Files
+	Bookmarks Bookmarks `cmd:"" group:"Manage Files" aliases:"bk" help:"Named aliases for often-used files"`
+	Bookmark  Bookmarks `cmd:"" hidden:"" help:"(Alias)"` // Hidden alias for convenience / typo
+	Edit      Edit      `cmd:"" group:"Manage Files" help:"Opens a file or bookmark in your editor"`
+	Goto      Goto      `cmd:"" group:"Manage Files" help:"Opens the file explorer at a file or bookmark"`
 
 	// Misc
-	Edit       Edit             `cmd:"" group:"Misc" help:"Opens a file or bookmark in your editor"`
-	Goto       Goto             `cmd:"" group:"Misc" help:"Opens the file explorer at the given location"`
+	Version    Version          `cmd:"" group:"Misc" help:"Prints version info and check for updates"`
+	Info       Info             `cmd:"" group:"Misc" default:"withargs" help:"Displays meta info about klog"`
 	Config     Config           `cmd:"" group:"Misc" help:"Prints the current configuration"`
 	Json       Json             `cmd:"" group:"Misc" help:"Converts records to JSON"`
-	Info       Info             `cmd:"" group:"Misc" default:"withargs" help:"Displays meta info about klog"`
-	Version    Version          `cmd:"" group:"Misc" help:"Prints version info and check for updates"`
 	Completion kcmpl.Completion `cmd:"" group:"Misc" help:"Outputs shell code for enabling tab completion"`
 }
